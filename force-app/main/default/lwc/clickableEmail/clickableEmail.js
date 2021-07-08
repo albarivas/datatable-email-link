65cc1177cce0932c3759e30b91761fc40fb01189
Alba Rivas <arivas@salesforce.com>
2021-07-08 14:29:13 +0200

    Use navigation service

--- force-app/main/default/lwc/clickableEmail/clickableEmail.js
+++ force-app/main/default/lwc/clickableEmail/clickableEmail.js
@@ -1,10 +1,20 @@
 import { LightningElement, api } from 'lwc';
-
-export default class ClickableEmail extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+export default class ClickableEmail extends NavigationMixin(
+  LightningElement
+) {
   @api recordId;
   @api email;
 
-  get recordIdURL() {
-    return `/${this.recordId}`;
+  navigate(event) {
+      event.preventDefault();
+      this[NavigationMixin.GenerateUrl]({
+          type: 'standard__recordPage',
+          attributes: {
+              recordId: this.recordId,
+              actionName: 'view'
+          }
+      }).then(url => { window.open(url) });
   }
 }
+
